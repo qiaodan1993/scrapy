f358524799b0489effdcc9910b78f33aeff77398
qiaodan <437645594@qq.com>
2020-12-18 11:36:28 +0800

    贵州

---
+++ tender/spiders/guizhou_zhaobiao.py
@@ -0,0 +1,41 @@
+import scrapy
+from tender.items import TenderItem
+
+class GuizhouZhaobiaoSpider(scrapy.Spider):
+    name = 'guizhou_zhaobiao'
+    allowed_domains = ['www.ccgp-guizhou.gov.cn']
+    start_urls = ['http://www.ccgp-guizhou.gov.cn/sjbx/']
+
+    province = '贵州'
+    typical = '招标'
+
+    def start_requests(self):
+
+        next_page = self.settings['COMMAND_NEXT_PAGE']
+        max_page = self.settings['COMMAND_MAX_PAGE']
+        for pageNum in range(next_page, max_page):
+            if pageNum == 1:
+                yield scrapy.Request(self.start_urls[0]+ 'index.html', self.parse, dont_filter=True)
+            else:
+                yield scrapy.Request(self.start_urls[0]+ 'index_' + str(pageNum) + '.html', self.parse, dont_filter=True)
+    def parse(self, response):
+        for row_data in (response.xpath('//*[@class="xnrx"]/ul/li')):
+
+            url = response.urljoin(row_data.css('li a::attr(href)').get())
+            item = TenderItem()
+            item['url'] = url
+            item['publish_at'] = row_data.xpath('span/text()')[0].extract()
+            item['province'] = self.province
+            item['typical'] = self.typical
+            request = scrapy.Request(url, callback=self.parse_detail, dont_filter=True)
+            request.meta['item'] = item
+
+            yield request
+        pass
+    def parse_detail(self, response):
+        item = response.meta['item']
+        item['title'] = response.xpath('//h3/text()').get()
+        item['content'] = response.xpath('//div[@id="border"]').get()
+        item['html_source'] = response.body
+
+        yield item
---
+++ tender/spiders/guizhou_zhongbiao.py
@@ -0,0 +1,44 @@
+import scrapy
+from tender.items import TenderItem
+
+class GuizhouZhongbiaoSpider(scrapy.Spider):
+    name = 'guizhou_zhongbiao'
+    allowed_domains = ['www.ccgp-guizhou.gov.cn']
+
+    start_urls = ['http://www.ccgp-guizhou.gov.cn/sjbx/zbcggg/']
+
+    province = '贵州'
+    typical = '中标'
+
+    def start_requests(self):
+
+        next_page = self.settings['COMMAND_NEXT_PAGE']
+        max_page = self.settings['COMMAND_MAX_PAGE']
+        for pageNum in range(next_page, max_page):
+            if pageNum == 1:
+                yield scrapy.Request(self.start_urls[0] + 'index.html', self.parse, dont_filter=True)
+            else:
+                yield scrapy.Request(self.start_urls[0] + 'index_' + str(pageNum) + '.html', self.parse,
+                                     dont_filter=True)
+
+    def parse(self, response):
+        for row_data in (response.xpath('//*[@class="xnrx"]/ul/li')):
+            url = response.urljoin(row_data.css('li a::attr(href)').get())
+            item = TenderItem()
+            item['url'] = url
+            item['publish_at'] = row_data.xpath('span/text()')[0].extract()
+            item['province'] = self.province
+            item['typical'] = self.typical
+            request = scrapy.Request(url, callback=self.parse_detail, dont_filter=True)
+            request.meta['item'] = item
+
+            yield request
+        pass
+
+    def parse_detail(self, response):
+        item = response.meta['item']
+        item['title'] = response.xpath('//h3/text()').get()
+        item['content'] = response.xpath('//div[@id="border"]').get()
+        item['html_source'] = response.body
+
+        yield item
