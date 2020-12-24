6afe1f1ca3e7348ccd2061752833bfa6e39a26ed
启诺data-ebaina机器 <njqinuo@data-ebaina.net>
2020-12-24 15:49:41 +0800

    浙江详情修改

--- tender/spiders/zhejiang_zhongbiao.py
+++ tender/spiders/zhejiang_zhongbiao.py
@@ -3,11 +3,14 @@ import json
 from tender.items import TenderItem 
 import time
 import re
+import urllib.parse as urlparse
+import requests
 
 class ZhejiangZhongBiaoSpider(scrapy.Spider):
     name = 'zhejiang_zhongbiao'
     allowed_domains = ['czt.zj.gov.cn']
     start_urls = ['https://zfcgmanager.czt.zj.gov.cn/cms/api/cors/remote/results?pageSize=15&sourceAnnouncementType=3004%2C4005%2C4006&url=notice']
+    detail_url = 'https://zfcgmanager.czt.zj.gov.cn/cms/api/cors/remote/results?noticeId={noticeId}&url=noticeDetail'
 
     province = '浙江'
     typical = '中标'
@@ -24,7 +27,10 @@ class ZhejiangZhongBiaoSpider(scrapy.Spider):
         for row_data in js["articles"]:
 
             item = TenderItem()
-            item['url'] = row_data["url"]
+            url = row_data["url"]
+            url = url.replace('http', 'https')
+            item['url'] = url
+
             timeArray = time.localtime(int(row_data["pubDate"])/1000)
             item['publish_at'] = time.strftime("%Y-%m-%d", timeArray)
             item['province'] = self.province
@@ -38,8 +44,23 @@ class ZhejiangZhongBiaoSpider(scrapy.Spider):
     
     def parse_detail(self, response):
         item = response.meta['item']
+
+        # 获取一下noticeid
+        parsed = urlparse.urlparse(item['url'])
+        querys = urlparse.parse_qs(parsed.query)
+        noticeId = querys['noticeId'][0]
+        detial_url = self.detail_url.replace('{noticeId}', noticeId)
+
+        # 获取一下详情
+        detail_info = requests.get(detial_url)
+        content = (detail_info.json()['noticeContent'])
+
+        # 去掉一些标签
+        re_style = re.compile('<\s*style[^>].*>[^<]*<\s*/\s*style\s*>', re.I)
+        content = re_style.sub('', content)
         re_style = re.compile('<\s*a[^>].*>[^<]*<\s*/\s*a\s*>', re.I)
-        content = response.xpath('//div[@class="gpoz-detail-content"]').get()
-        item['content'] = re_style.sub('', content) # 去掉a标签
+        content = re_style.sub('', content)
+
+        item['content'] = content
         item['html_source'] = response.body
         yield item
