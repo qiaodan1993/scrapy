7897777ed5374c5be97f58f4e0ae58ea9e06b527
qiaodan <437645594@qq.com>
2020-12-29 15:49:38 +0800

    湖北

---
+++ tender/spiders/hubei_zhongbiao.py
@@ -0,0 +1,43 @@
+import scrapy
+from tender.items import TenderItem
+import re
+
+class HubeiZhongbiaoSpider(scrapy.Spider):
+    name = 'hubei_zhongbiao'
+    allowed_domains = ['www.ccgp-hubei.gov.cn']
+    start_urls = ['http://www.ccgp-hubei.gov.cn/']
+
+    province = '湖北'
+    typical = '中标'
+
+    def start_requests(self):
+        next_page = self.settings['COMMAND_NEXT_PAGE']
+        max_page = self.settings['COMMAND_MAX_PAGE']
+        for pageNum in range(next_page, max_page):
+            yield scrapy.Request(self.start_urls[0] + '/notice/cggg/pzhbgg/index_' + str(pageNum) + '.html', self.parse,
+                                 dont_filter=True)
+
+    def parse(self, response):
+        for row_data in response.xpath('//div[@class="news-list list-page"]/ul/li'):
+            url = response.urljoin(row_data.css('li a::attr(href)').get())
+
+            item = TenderItem()
+            item['url'] = url
+            item['publish_at'] = row_data.xpath('span/text()')[0].extract()
+            item['province'] = self.province
+            item['typical'] = self.typical
+            request = scrapy.Request(url, callback=self.parse_detail, dont_filter=True)
+            request.meta['item'] = item
+            yield request
+
+    def parse_detail(self, response):
+        item = response.meta['item']
+        item['title'] = response.xpath('//h2/span/text()').get()
+
+        content = response.xpath('//div[@class="art_con"]/div[2]').get()
+        re_style = re.compile('<\s*a[^>].*>[^<]*<\s*/\s*a\s*>', re.I)
+        content = re_style.sub('', content)
+        item['content'] = content  # 去掉a标签
+
+        item['html_source'] = response.body
+        yield item
